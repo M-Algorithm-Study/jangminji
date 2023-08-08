cf04be6b44ba1353d9566ee7522e2826192e0926
mungjimangji <mungjmangji@hphk.com>
2023-08-08 09:42:08 +0900

    [UPDATE] Week06

--- Week06/백준_15663.py
+++ Week06/백준_15663.py
@@ -1,5 +1,24 @@
-# import sys
-# sys.stdin  = open('input.txt', 'r')
-# N, M = map(int, input().split())
-# num_list = list(map(int, input().split()))
-# num_list.sort()
+import sys
+sys.stdin  = open('input.txt', 'r')
+N, M = map(int, input().split())
+num_list = list(map(int, input().split()))
+num_list.sort() # 1 7 9 9
+visited = [False]*(N+1)
+
+def backTracking(result):
+    prev = 0
+    if len(result) == M:      
+        print(*result)
+        return
+    
+    for i in range(len(num_list)):
+        if len(result) == 0 or visited[i] == False and num_list[i] != prev and num_list[i] >= result[-1]:
+
+            prev = num_list[i]
+            visited[i] = True
+            result.append(num_list[i])
+            backTracking(result)
+            visited[i] = False
+            result.pop()
+backTracking([])
+
--- Week06/프로그래머스_43162.py
+++ Week06/프로그래머스_43162.py
@@ -5,18 +5,16 @@ def solution(n, computers):
         if visited[i] == False:
             stack = [i]
             visited[i] = True
-            while stack:
+            while stack: # 연결된 컴퓨터는 이 while문에서 모두 true로 바뀜
                 cur = stack.pop()
                 for j in range(n):# 0 1 2
                     if cur == j:
                         continue
                     elif computers[cur][j] == 1 and visited[j] == False:
                         visited[j] = True
-                        
                         stack.append(j)
             answer += 1        
-        
-
+    
     return answer
 
 print(solution(3, [[1, 1, 0], [1, 1, 0], [0, 0, 1]]))
