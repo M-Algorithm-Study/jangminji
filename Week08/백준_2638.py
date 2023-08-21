34b85906736a3effe634f869a155e86d214ef83c
mungjimangji <mungjmangji@hphk.com>
2023-08-22 02:04:06 +0900

    [UPDATE] Week08 치즈 문제 변수 수정, 주석 추가

--- Week08/백준_2638.py
+++ Week08/백준_2638.py
@@ -1,45 +1,50 @@
-# N,M = map(int, input().split())
-
-# graph = []
-# for _ in range(N):
-#     graph.append(list(map(int, input().split())))
-
-# for i in range(M):
-#     for j in range(N):
-#         if graph[i][j] == 1:
-
 from collections import deque
 
-n, m = map(int, input().split())
-cheeze = [list(map(int, input().split())) for _ in range(n)]
-dx, dy = [0, 0, 1, -1], [1, -1, 0, 0]
-ans = 0
-while True:
-    queue = deque()
-    ch = [[0 for _ in range(m)] for _ in range(n)]
-    ch[0][0] = 1
-    queue.append([0, 0])
-    while queue:
-        x, y = queue.popleft()
-        for k in range(4):
-            nx, ny = x+dx[k], y+dy[k]
-            if 0 <= nx < n and 0 <= ny < m and not ch[nx][ny]:
-                if cheeze[nx][ny]:
-                    cheeze[nx][ny] += 1
-                else:
-                    ch[nx][ny] = 1
-                    queue.append([nx, ny])
+N, M = map(int, input().split())
+cheeze = [list(map(int, input().split())) for _ in range(N)]
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+days = 0
 
-    flag = 0
-    for i in range(n):
-        for j in range(m):
+while True:
+    # 새로운 날
+    stack = deque()
+    visited = [[0 for _ in range(M)] for _ in range(N)]
+    visited[0][0] = 1
+    stack.append([0, 0])
+
+    # 공기를 while문으로 연결된 부분 전체 탐색 (자동으로 stack에 담기지 않음 )
+    while stack:
+        x, y = stack.popleft()
+        for k in range(4):
+            nx = x + dx[k]
+            ny = y + dy[k]
+            # 범위 안에 있는 경우 중에서
+            if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny]:
+                # 치즈인 경우와
+                if cheeze[nx][ny] != 0:
+                    cheeze[nx][ny] += 1
+                # 공기인 경우에만 다시 방문하지 않기 위해 방문리스트에 표시해주고, 사방으로 돌기 위해서 stack에 넣어준다.
+                else:
+                    visited[nx][ny] = 1
+                    stack.append([nx, ny])
+
+    # 상한 치즈 확인하는 코드
+    check = 0
+    for i in range(N):
+        for j in range(M):
+            # 2개의 면적이 공기 중에 닿아서 +2가 된 경우 3이 된다.
+            # 이 경우엔 치즈를 0으로 초기화 시켜준다.
             if cheeze[i][j] >= 3:
                 cheeze[i][j] = 0
-            elif 0 < cheeze[i][j]:
-                flag = 1
+            
+            # 1인 경우나 2인 경우일 경우 (외부와 닿는 면적이 2개 미만인 치즈들)
+            elif 2 == cheeze[i][j] or 1 == cheeze[i][j]:
+                check = 1
                 cheeze[i][j] = 1
-    ans += 1
+    days += 1
 
-    if not flag:
-        print(ans)
+    # 상한 치즈를 확인하는 부분에서 치즈인 경우는 flag가 1이 변하는데 변하지 않는 경우는 while문 종료
+    if check == 0:
+        print(days)
         break
