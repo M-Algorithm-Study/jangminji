8588a68acff1c4ecb51a27d59b0ead0228e77f4e
mungjimangji <mungjmangji@hphk.com>
2023-09-19 18:43:44 +0900

    [UPDATE] TIL 마저 작성하기 위해서 표시 해둠

--- Week11/백준_9251.py
+++ Week11/백준_9251.py
@@ -1,20 +1,31 @@
 import sys
 sys.stdin  = open('input.txt', 'r')
-# a문자와 b문자의 부분 수열의 몇번째는 중요하지 않음
-# 그럼 만약에 먼저 나온 부분 수열을 무시하고 뒤에 나온애를 선택해도 되나?
-# ㄴㄴ 안됨
-# 두 단어의 문자수는 항상 같은가? ㄴㄴ
 
-word1= input()
-word2= input()
-l1, l2 = len(word1), len(word2)
-cache = [0] * l1
+# word1= input() # ACAYKP
+# word2= input() # CAPCAK
+# l1, l2 = len(word1), len(word2)
+# word1_list = [0] * l1
 
-for i in range(l2):
-    cnt = 0
-    for j in range(l1):
-        if cnt < cache[j]:
-            cnt = cache[j]
-        elif word1[j] == word2[i]:
-            cache[j] = cnt + 1
-print(max(cache))
+# for i in range(l2):
+#     cnt = 0
+#     for j in range(l1):
+#         if cnt < word1_list[j]:
+#             cnt = word1_list[j]
+#         elif word1[j] == word2[i]:
+#             word1_list[j] = cnt + 1
+# print(max(word1_list))
+
+
+word = input()
+word_1 = input()
+x = 0
+a = []
+
+for i in range(len(word)):
+    for j in range(x , len(word_1)):
+        if word[i] == word_1[j]:
+            a.append(word[i])
+            x = j
+            break
+
+print(len(a))
