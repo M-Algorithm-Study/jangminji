3128f6675b951a4a7cfc8de292cc7607341e9841
mungjimangji <mungjmangji@hphk.com>
2023-09-25 17:13:56 +0900

    [ADD] Week13 13주차 추가 11주 수정

--- Week11/백준_9251.py
+++ Week11/백준_9251.py
@@ -1,31 +1,16 @@
 import sys
 sys.stdin  = open('input.txt', 'r')
 
-# word1= input() # ACAYKP
-# word2= input() # CAPCAK
-# l1, l2 = len(word1), len(word2)
-# word1_list = [0] * l1
+word1= input() # ACAYKP
+word2= input() # CAPCAK
+l1, l2 = len(word1), len(word2)
+word1_list = [0] * l1
 
-# for i in range(l2):
-#     cnt = 0
-#     for j in range(l1):
-#         if cnt < word1_list[j]:
-#             cnt = word1_list[j]
-#         elif word1[j] == word2[i]:
-#             word1_list[j] = cnt + 1
-# print(max(word1_list))
-
-
-word = input()
-word_1 = input()
-x = 0
-a = []
-
-for i in range(len(word)):
-    for j in range(x , len(word_1)):
-        if word[i] == word_1[j]:
-            a.append(word[i])
-            x = j
-            break
-
-print(len(a))
+for i in range(l2):
+    cnt = 0
+    for j in range(l1):
+        if cnt < word1_list[j]:
+            cnt = word1_list[j]
+        elif word1[j] == word2[i]:
+            word1_list[j] = cnt + 1
+print(max(word1_list))
