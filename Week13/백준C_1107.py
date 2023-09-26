224d7cff48adc61c19c4288a43af3a4991ebc357
mungjimangji <mungjmangji@hphk.com>
2023-09-26 12:33:20 +0900

    [UPDATE] Week13 공통 문제 주석 추가

--- Week13/백준C_1107.py
+++ Week13/백준C_1107.py
@@ -1,12 +1,12 @@
 n = int(input())
 remoteNum = [x for x in range(10)]
-breakdownButtonNum = int(input())
+b_Button = int(input())
 
 #만약 고장난 버튼이 0개 이상이라면
-if breakdownButtonNum > 0:
-    breakdownButtons = list(map(int, input().split()))
+if b_Button > 0:
+    b_Button_list = list(map(int, input().split()))
     # 고장난 리모컨 버튼 제거
-    for i in breakdownButtons:
+    for i in b_Button_list:
         remoteNum.remove(i)
 
 #우선 현재 채널과 입력받은 채널의 차의 절대값을 결과를 출력할 변수에 대입
@@ -15,7 +15,7 @@ result = abs(100 - n)
 #0번부터 999999번까지 채널을 순회한다.(999999까지 도는이유는 만약 버튼을 8,9만 눌릴수 있다고 쳤을 때 500000채널까지 가기위해서는
 #99999 에서 가는것보다 888888에서 내려가는 것이 더가깝기 때문이다.)
 for channel in range(1000000):
-    for j in range(len(str(channel))): #각 채널을 문자열로 변환해서 그 길이만큼 돈다.
+    for j in range(len(str(channel))): #각 채널을 '문자열'로 변환해서 그 길이만큼 돈다.
         if int(str(channel)[j]) not in remoteNum: #만약 채널의 각각의 자리수가 리모컨의 고장나 있는 버튼이라면
             break #해당 반복문 탈출(고장난 버튼이 포함되어있는 채널)
         elif len(str(channel)) - 1 == j: #만약 해당채널의 마지막 자리수까지 위에서 검사를 했다면
--- Week13/백준C_21736.py
+++ Week13/백준C_21736.py
@@ -24,15 +24,20 @@ q = deque()
 q.append((d_y,d_x))
 person = 0
 
+# 상하좌우로 이동하기
 while q:
     y, x = q.popleft()
     for n in range(4):
         n_x = x + dx[n]
         n_y = y + dy[n]
+        # 상하좌우가 범위 안에 있고,
         if 0 <= n_x < M and 0 <= n_y < N:
+            # X가 아닌 경우
             if uni[n_y][n_x] != "X":
+                # 그 중에서 P인 경우에는 변수에 +1
                 if uni[n_y][n_x] == "P":
                     person += 1
+                # 상하좌우 범위 안에 있고 X가 아니면 q에 담아줌 그리고 다시 돌지 않기 위해서 X로 만들어줌
                 q.append((n_y, n_x))
                 uni[n_y][n_x] = "X"
 if person:
