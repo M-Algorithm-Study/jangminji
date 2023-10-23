d274ff21b12115d4a2d4596883dfc978d97d3c47
mungjimangji <mungjmangji@hphk.com>
2023-10-23 16:33:10 +0900

    [UPDATE] Week 17 백준 공통문제 2565번 주석 달기

--- Week17/백준C_2565.py
+++ Week17/백준C_2565.py
@@ -8,8 +8,10 @@ for _ in range(n):
     a, b = map(int, input().split())
     electrics.append((a, b))
 
+# 정렬 a값 기준으로 정렬
 electrics.sort(key=lambda x: (x[0]))
 
+# 가장 긴 수열 만들기로 코드를 작성
 for i in range(n):
     for j in range(i):
         if electrics[j][1] < electrics[i][1]:
