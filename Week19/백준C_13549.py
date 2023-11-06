0b9e42693da479e78bd56cba1e866ad17c246cf0
mungjimangji <mungjmangji@hphk.com>
2023-11-06 13:34:11 +0900

    [UPDATE] Week19

--- Week19/백준C_13549.py
+++ Week19/백준C_13549.py
@@ -2,21 +2,23 @@ from collections import deque
 
 N, K = map(int, input().split())
 visited = [-1] * 100001
-stack = deque([N])
+q = deque([N])
 visited[N] = 0
-
-while stack:
-    cur = stack.popleft()
+cnt = 0
+while q:
+    cur = q.popleft()
+    cnt += 1
+    print("cnt", cnt, "cur", cur)
     if cur == K:
         print(visited[cur])
         break
     for i in (cur * 2, cur + 1, cur - 1):
-        if 0 <= i <= 100000 and visited[i] == -1:
+        if 0 <= i <= 100000 and (visited[i] == -1 or visited[i] > visited[cur]):
             if i == (cur * 2):
                 visited[i] = visited[cur]
-                stack.appendleft(i)
+                q.appendleft(i)
                 
             else:
                 visited[i] = visited[cur] + 1
-                stack.append(i)
-# 왜 틀렸다고 나와요.....ㅜㅠ
+                q.append(i)
+# 왜 틀렸다고 나와요.....ㅜㅠ 해결!
